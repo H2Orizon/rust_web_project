3aa46b9e8432ba63fcdafb8c3f2e9a2d77d80cae
H2Orizon <ferents1vladeslav@gmail.com>
2025-03-27 17:00:47 +0200

    add hashing to password

--- src/services/user_service.rs
+++ src/services/user_service.rs
@@ -1,4 +1,6 @@
-use crate::{db, models::user_model::{ActiveModel, NewUserForm}};
+use crate::models::user_model::{ActiveModel, NewUserForm};
+use argon2::{Argon2, PasswordHasher};
+use argon2::password_hash::{SaltString, rand_core::OsRng};
 use sea_orm::{Set, ActiveModelTrait, DatabaseConnection};
 use thiserror::Error;
 
@@ -9,11 +11,16 @@ pub enum UserError {
 }
 
 pub async fn creat_user(db: &DatabaseConnection, form_data: &NewUserForm) -> Result<(), UserError>{
+    let salt = SaltString::generate(&mut OsRng);
+    let argon = Argon2::default();
+    let password_hash = argon.hash_password(form_data.password.as_bytes(), &salt)
+    .map_err(|_| UserError::DatabaseError(sea_orm::DbErr::Custom("Password hashing failed".to_string())))?
+    .to_string();
     let new_user = ActiveModel{
         username: Set(form_data.username.clone()),
         email: Set(form_data.email.clone()),
         phone_num: Set(form_data.phone_num.clone()),
-        password: Set(form_data.password.clone()),
+        password: Set(password_hash),
         role: Set(form_data.role.clone()),
         ..Default::default()
     };
